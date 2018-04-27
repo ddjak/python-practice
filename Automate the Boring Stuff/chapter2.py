c8f38b6f12e8e7cb7484c9ff912eb51bc986d658
admin <drdjakovic@gmail.com>
2018-04-27 13:53:56 -0400

    chapter 2

---
+++ Automate the Boring Stuff/chapter2.py
@@ -0,0 +1,16 @@
+#CHAPTER 2
+#while loops and code blocks
+
+'''
+while True:
+	print("Who are you?")
+	name = input()
+	if name != "Joe":
+		continue
+	print("Hey, Joe. What's your password?")
+	password = input()
+	if password == "swordfish":
+		break
+print("Acess granted")
+'''
+
